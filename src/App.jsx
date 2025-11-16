9c188bc4b41bc44c97c1718610fafc16400892ba
kelvin-N <nkelvin781@gmail.com>
2025-11-16 23:51:10 +0000

    modify chnages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Counter from "./components/Counter";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
