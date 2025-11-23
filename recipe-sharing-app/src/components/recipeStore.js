d79a50ba9a8715d94b38a8cc36535a3513b4db0d
kelvin-N <nkelvin781@gmail.com>
2025-11-23 02:39:29 +0000

    add changes

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,10 +1,15 @@
 import { create } from "zustand";
 
 export const useRecipeStore = create((set) => ({
+  // The list of recipes
   recipes: [],
 
+  // Add a new recipe
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
+
+  // Replace the entire recipes list
+  setRecipes: (recipes) => set({ recipes }),
 }));
