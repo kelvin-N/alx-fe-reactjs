cb470a415b7b57d88541f74231e6a93a9173c109
kelvin-N <nkelvin781@gmail.com>
2025-11-23 02:16:27 +0000

    Add recipe-sharing-app as normal folder

---
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useRecipeStore } from "../store/recipeStore";
+
+function AddRecipeForm() {
+  const addRecipe = useRecipeStore((state) => state.addRecipe);
+
+  const [title, setTitle] = useState("");
+  const [ingredients, setIngredients] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!title.trim() || !ingredients.trim()) return;
+
+    addRecipe({ title, ingredients });
+
+    setTitle("");
+    setIngredients("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Add Recipe</h2>
+
+      <input
+        type="text"
+        placeholder="Recipe title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <textarea
+        placeholder="Ingredients"
+        value={ingredients}
+        onChange={(e) => setIngredients(e.target.value)}
+      />
+
+      <button type="submit">Add Recipe</button>
+    </form>
+  );
+}
+
+export default AddRecipeForm;
