6ce659facea59cc070adfe339d444f5eb4ebbe0c
kelvin-N <nkelvin781@gmail.com>
2025-12-01 21:00:27 +0000

    add changes

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./index.html",         // root HTML file
+    "./public/index.html",  // include public folder just in case
+    "./src/**/*.{js,jsx,ts,tsx}" // all JS/TS/JSX/TSX files in src
   ],
   theme: {
     extend: {},
