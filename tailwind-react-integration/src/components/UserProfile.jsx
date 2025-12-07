75afdf67862dd243707a582b237d8958d09fc18c
kelvin-N <nkelvin781@gmail.com>
2025-12-07 13:21:48 +0000

    add changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 const UserProfile = () => {
   return (
-    <div className="bg-gray-100 p-8 my-20 rounded-lg max-w-md mx-auto text-center">
+    <div className="bg-gray-100 p-8 my-20 rounded-lg max-w-sm mx-auto text-center shadow-lg">
 
       <img
         src="https://via.placeholder.com/150"
