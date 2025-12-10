179c47715f6159cc3dd3c5aa03ae0d0087c44f1d
kelvin-N <nkelvin781@gmail.com>
2025-12-10 18:25:13 +0000

    add changes

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,18 +1,35 @@
-import { Outlet, Link, useParams } from "react-router-dom";
+import { Link, Routes, Route, Outlet, useParams } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 function Profile() {
-  const { userId } = useParams(); // For dynamic routing
+  const { userId } = useParams(); // dynamic route parameter
 
   return (
     <div>
-      <h1>Profile Page {userId ? `- User ${userId}` : ""}</h1>
-      
-      <nav style={{ marginBottom: "1rem" }}>
-        <Link to="details" style={{ marginRight: "1rem" }}>Details</Link>
-        <Link to="settings">Settings</Link>
+      <h1>Profile Page</h1>
+      <p>User ID: {userId}</p>
+
+      {/* Navigation inside profile */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
       </nav>
 
-      <Outlet /> {/* Nested routes will render here */}
+      {/* Nested routing */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+
+      {/* OR using Outlet (also allowed) */}
+      <Outlet />
     </div>
   );
 }
