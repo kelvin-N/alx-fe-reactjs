32ae14a6f7cd88f3a81833c5af913f25c3493a1a
kelvin-N <nkelvin781@gmail.com>
2025-12-15 19:16:24 +0000

    add changes

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>© 2023 City Lovers. All rights reserved.</p> {/* Checker-compliant text */}
+      <p>© 2023 City Lovers. All rights reserved.</p>
     </footer>
   );
 };
