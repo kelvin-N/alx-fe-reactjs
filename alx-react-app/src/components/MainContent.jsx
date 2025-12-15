40916cd29769af596a9ddc51913cc25a40c39b1b
kelvin-N <nkelvin781@gmail.com>
2025-12-15 18:15:47 +0000

    add changes

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -4,7 +4,7 @@ const MainContent = () => {
   return (
     <main>
       <h2>My Favorite Cities</h2>
-      <p>I love to visit New York, Paris, and Tokyo.</p> {/* Checker-compliant text */}
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 };
