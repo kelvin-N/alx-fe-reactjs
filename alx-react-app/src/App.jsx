997447c0dc2540340b7a30191c5523a3e4afca2e
kelvin-N <nkelvin781@gmail.com>
2025-12-15 18:56:10 +0000

    add changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import UserProfile from "./components/UserProfile";
 
 function App() {
-  const user = {
-    name: "Kelvin N.",
-    age: 20,
-    bio: "I am learning React at ALX.",
-  };
-
   return (
     <div>
       <h1>Welcome to the User Dashboard</h1>
-      <UserProfile name={user.name} age={user.age} bio={user.bio} />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
     </div>
   );
 }
