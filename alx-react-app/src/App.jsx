e2a5bf1c7bb7fe19874b3f0d8d3a610bd960c438
kelvin-N <nkelvin781@gmail.com>
2025-12-15 19:11:34 +0000

    add changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
     <div>
-      <h1>Welcome to the User Dashboard</h1>
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
+      <WelcomeMessage />
     </div>
   );
 }
--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const WelcomeMessage = () => {
   return (
     <div>
-      <h1>Hello everyone, I am learning React at ALX!</h1> {/* Checker-compliant h1 */}
-      <p>I am learning about JSX!</p> {/* Checker-compliant p */}
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>I am learning about JSX!</p>
     </div>
   );
 };
