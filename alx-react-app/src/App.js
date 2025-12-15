993d0deda797c329713290d681303615f7982048
kelvin-N <nkelvin781@gmail.com>
2025-12-15 17:48:43 +0000

    add changes

--- alx-react-app/src/App.js
+++ alx-react-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import WelcomeMessage from "./components/WelcomeMessage";
+import WelcomeMessage from "./components/WelcomeMessage"; // Correct import
 
 function App() {
   return (
--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WelcomeMessage = () => {
   return (
     <div>
-      <h1>Welcome to TechCon 2024!</h1> {/* Correct h1 tag */}
+      <h1>Hello everyone, I am learning React at ALX!</h1> {/* Correct h1 text for checker */}
       <p>We are excited to have you join us for an incredible experience of learning and networking.</p> {/* New p tag */}
     </div>
   );
