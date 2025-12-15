549657e7aa9470838058b80e1b330f33938787d7
kelvin-N <nkelvin781@gmail.com>
2025-12-15 17:38:07 +0000

    add changes

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage /> {/* Imported and used here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WelcomeMessage = () => {
+  return (
+    <div>
+      <h1>Welcome to TechCon 2024!</h1> {/* Correct h1 tag */}
+      <p>We are excited to have you join us for an incredible experience of learning and networking.</p> {/* New p tag */}
+    </div>
+  );
+};
+
+export default WelcomeMessage;
